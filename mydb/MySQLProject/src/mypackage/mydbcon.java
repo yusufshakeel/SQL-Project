cdf493384bbfc8972b1c444e0ce2ca7963166493
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-03-12 20:37:32 +0530

    Initial commit

---
+++ mydb/MySQLProject/src/mypackage/mydbcon.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+
+public class mydbcon {
+	public static final String jdbcDriver = "com.mysql.jdbc.Driver";
+	public static final String dburl = "jdbc:mysql://localhost/mydb";
+	public static final String username = "root";
+	public static final String password = "root123";
+}
