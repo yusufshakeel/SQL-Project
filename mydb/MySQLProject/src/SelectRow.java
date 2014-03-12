e022a7a1920548289b70ea60afd8061599c151e0
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-03-12 20:37:26 +0530

    Initial commit

---
+++ mydb/MySQLProject/src/SelectRow.java
@@ -0,0 +1,58 @@
+import mypackage.mydbcon;
+import java.sql.*;
+
+public class SelectRow {
+	public static void all(){
+		//variables
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try{
+			//register jdbc
+			Class.forName(mydbcon.jdbcDriver);
+			
+			//get connection
+			con = DriverManager.getConnection(mydbcon.dburl, mydbcon.username, mydbcon.password);
+			
+			//create and execute query
+			String sqlquery = "select id, firstName, lastName, dob, gender from customer";
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sqlquery);
+			
+			//display result
+			while(rs.next()){
+				int id = rs.getInt("id");
+				String fname = rs.getString("firstName");
+				String lname = rs.getString("lastName");
+				String dob = rs.getString("dob");
+				String gender = rs.getString("gender");
+				
+				System.out.println("ID: "+id);
+				System.out.println("First name: "+fname);
+				System.out.println("Last name: "+lname);
+				System.out.println("DOB: "+dob);
+				System.out.println("Gender: "+gender);
+			}
+			
+			//close resources
+			rs.close();
+			stmt.close();
+			con.close();
+		}catch(Exception e){
+			System.out.println("Error: "+e);
+		}finally{
+			try{
+				if(stmt!=null){
+					stmt.close();
+				}
+				if(con!=null){
+					con.close();
+				}
+				if(rs!=null){
+					rs.close();
+				}
+			}catch(Exception e){}
+		}
+	}//all() method ends here
+}//SelectRow class ends here
