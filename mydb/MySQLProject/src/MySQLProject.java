137bdf01595a2e1e00f17e6164ba01589631e8d3
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-03-12 20:37:20 +0530

    Initial commit

---
+++ mydb/MySQLProject/src/MySQLProject.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)MySQLProject.java
+ *
+ * MySQLProject application
+ *
+ * @author 
+ * @version 1.00 2014/3/12
+ */
+ 
+public class MySQLProject {
+    
+    public static void main(String[] args) {
+    	SelectRow.all();
+    }
+}
